a6259cf3a9038795db97472dfb5182885020ad72
Will Foran <foranw@upmc.edu>
2012-03-09 16:19:02 -0500

    %data is for prevSubj not subj!

--- subj_cond_phase.pl
+++ subj_cond_phase.pl
@@ -29,12 +29,15 @@ while(<$allFH>) {
  chomp;
  # get what we need from the file
  ($subj, $cond, $type, $longtype, @rest) = split /\t/;
- $rest[6] =~s/'//g;
+ $rest[6] =~s/'//g if $rest[6];
 
 
  # new subject
   if($prevSubj ne $subj ){
+
+   # write previous subjects data
    &writexls;
+
    # reset prevSubj
    $prevSubj=$subj;
    
@@ -44,7 +47,7 @@ while(<$allFH>) {
 
 }
 
-# get the last one
+# get the last (current) subject
 &writexls;
 
 
@@ -54,32 +57,39 @@ while(<$allFH>) {
 
 sub writexls {
 
-  print "new subject $subj\n";
-  return if $subj == 0;
+  return if $prevSubj == 0; # because 0 has no data
+
+  print "writing subj $prevSubj data\n";
 
   # open new xls
-  my $workbook = Spreadsheet::WriteExcel->new("xls/$subj.xls");
+  my $workbook = Spreadsheet::WriteExcel->new("xls/$prevSubj.xls");
 
+  # for each cond-type combo, make a new sheet
   for my $condType (sort keys %data) {
    my $worksheet = $workbook->add_worksheet("$condType");
 
+   # add header
    unshift @{$data{$condType}}, 
             ['Trial number', 'ROI ID', 'Mean X Coordinate of fixation', 
              'Mean Y coordinate of fixation', 'Latency to start of fix',
              'Latency to end of fix', 'Type of ROI'];
 
+   # for each row
    for my $row (0..$#{$data{$condType}}+1) {
 
+    # and each column
     for my $col (0..6) {
+
+       # write data
        $worksheet->write($row, $col, $data{$condType}[$row][$col] );
     }
+
    }
 
   }
 
   # close xls
   $workbook->close();
-
 
   # clear data
   %data=();
